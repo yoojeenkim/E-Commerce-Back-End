39838f7cc31361097ea2b83b09945d6ef169ee2c
daniel <yoojeenkim@gmail.com>
2021-10-21 01:24:37 -0400

    added category model

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,27 +7,42 @@ router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-    const tagsData = await Tag.findAll();
-    res.status(200).json(tagsData);
+    const tagData = await Tag.findAll();
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.idl, {
+      include: [{}]
+    });
+  
+    if (!tagData) {
+      res.status(404).json({ message: 'No location found with this id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/',async (req, res) => {
   // create a new tag
+
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
 });
 
